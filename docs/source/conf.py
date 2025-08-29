11d01b54c563e52139b4c762265c2ddeeeec22b1
psssantos <paulo.s.santos@inesctec.pt>
2025-08-29 10:02:12 +0100

    initial

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'NanoPolaris'
+copyright = '2025, INESCTEC'
+author = 'Paulo S. S. dos Samtps'
 
 release = '0.1'
 version = '0.1.0'
--- lumache.py
+++ lumache.py
@@ -1,5 +1,5 @@
 """
-Lumache - Python library for cooks and food lovers.
+Nanopolaris - A tool for rapid computational optimization of plasmonic nanoparticles for chemical and biological sensing applications.
 """
 
 __version__ = "0.1.0"
